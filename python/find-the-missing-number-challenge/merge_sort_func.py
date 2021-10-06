420e65c2a7333d248e0d525fceb205dfde5437bf
AndreaOrlando23 <andrea.orlando87@gmail.com>
2021-10-06 18:39:05 +0200

    [FEAT] add merge_sort function

---
+++ python/find-the-missing-number-challenge/merge_sort_func.py
@@ -0,0 +1,37 @@
+def merge_sort(arr):
+    if len(arr) > 1:
+        left_arr = arr[:len(arr)//2]
+        right_arr = arr[len(arr)//2:]
+
+        # recursion
+        merge_sort(left_arr)
+        merge_sort(right_arr)
+
+        # merge
+        i = 0  # left array index
+        j = 0  # right array index
+        k = 0  # merged array index
+
+        while i < len(left_arr) and j < len(right_arr):
+            if left_arr[i] < right_arr[j]:
+                arr[k] = left_arr[i]
+                i += 1
+            else:
+                arr[k] = right_arr[j]
+                j += 1
+            k += 1  # increment k in both cases in the while loop
+        
+        while i < len(left_arr):
+            arr[k] = left_arr[i]
+            i += 1
+            k += 1
+        
+        while j < len(right_arr):
+            arr[k] = right_arr[j]
+            j += 1
+            k += 1
+
+
+my_list = [2, 3, 5, 1, 8, 7, 4, 6, 10, 9]
+merge_sort(my_list)
+print(my_list)
