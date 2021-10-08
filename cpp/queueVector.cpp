12a4a938fb1442174d6b147888a8db1fa9120d3b
AndreaOrlando23 <andrea.orlando87@gmail.com>
2021-10-08 19:27:49 +0200

    feat: add solution with vector for queue challenge

---
+++ cpp/queueVector.cpp
@@ -0,0 +1,131 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Queue 
+{
+    private:
+    vector<int> queue;
+
+    public:
+    // constructor
+    // Queue() {}
+
+    void enqueue(int item) 
+    {
+        queue.push_back(item);
+    }
+
+    int dequeue() 
+    {
+        int x = 0;
+        if(isEmpty()) 
+        {
+            cout << "Queue is EMPTY" << endl;
+            return -1;  //we have to reurn some value couse of the int function
+        }
+        else 
+        {
+            x = queue.at(0);
+            queue.erase(queue.begin());
+            return x;
+        }
+    }
+
+    int peek() 
+    {
+        if(!isEmpty()) {
+            return queue.at(0);
+        }
+    }
+
+    bool isEmpty() 
+    {
+        if(queue.size() == 0)
+            return true;
+        else
+            return false;
+    }
+
+    int size() 
+    {
+        return queue.size();
+    }
+
+    void display() 
+    {
+        if (isEmpty())
+        {
+            cout << "0";
+        }
+        for(int i=0; i<queue.size(); i++) 
+        {
+            cout << queue[i] << " ";
+        }
+    }
+
+};
+
+
+int main() 
+{
+    Queue test1;
+    int option, item;
+
+    do 
+    {
+        cout << "\n\nWhat operation do you want to perform? Select Option number (0 to exit)." << endl;
+        cout << "1. enqueue(item)" << endl;
+        cout << "2. dequeue()" << endl;
+        cout << "3. peek()" << endl;
+        cout << "4. isEmpty()" << endl;
+        cout << "5. size()" << endl;
+        cout << "6. display()" << endl;
+        
+
+        cin >> option;
+
+        switch(option) 
+        {
+            case 0:
+                break;
+
+            case 1:
+                cout << "*** Enqueue Operation 1*** \nPlease enter an item to Enqueue in the Queue:"<<endl;
+                cin >> item;
+                test1.enqueue(item);
+                break;
+
+            case 2:
+                cout << "*** Dequeue Operation *** \nDequeued Value: " << test1.dequeue() <<endl;
+                break;
+            
+            case 3:
+                cout << "*** Peek Operation *** \nThe first item is: " << test1.peek() <<endl;
+                break;
+            
+            case 4:
+                if(test1.isEmpty())
+                    cout << "Queue is Empty." << endl;
+                else
+                    cout << "Queue is NOT Empty." << endl;
+                break;
+            
+            case 5:
+                cout << "*** Size Operation *** \nCount of items in Queue: " << test1.size() <<endl;
+                break;
+            
+            case 6:
+                cout << "*** Display Function Called *** \nAll values in the Queue are: ";
+                test1.display();
+                break;
+            
+            default:
+                cout << "Please enter a valid option number. " << endl;
+                break;
+        }
+    }
+    while(option!=0);
+
+    return 0;
+}
