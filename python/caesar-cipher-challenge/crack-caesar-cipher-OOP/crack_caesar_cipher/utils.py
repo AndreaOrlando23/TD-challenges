c10e9e4fd519c325d80db9682ca0a07b20b2e2a2
AndreaOrlando23 <andrea.orlando87@gmail.com>
2021-10-27 18:56:43 +0200

    feat: implement some possible solution in utils file

--- python/caesar-cipher-challenge/crack-caesar-cipher-OOP/crack_caesar_cipher/utils.py
+++ python/caesar-cipher-challenge/crack-caesar-cipher-OOP/crack_caesar_cipher/utils.py
@@ -1,26 +1,34 @@
 
 
-def iterate_each_value(list_of_values):
-    for value in list_of_values:
-        return value
-
-
 def check_input(answer):
     if answer == "y":
         return True
     return False
 
+
+def ask_user():
+    answer = input("y/n: ")
+    if check_input(answer):
+        return answer
+    return False
+
+def return_first_value(list_of_values):
+    return list_of_values[0]
+
+
+def iterate_each_value(list_of_values):
+    return_first_value(list_of_values)
+    for value in list_of_values:
+        if ask_user():
+            return value
+        break
+    return value
+    
+
 def main():
-    test = input("y/n: ")
-    list_of_values = [1, 2, 3, 4, 5]
+    test = ask_user()
 
-    while check_input(test):
-        for value in list_of_values:
-            print(value)
-            test = input("y/n: ")
-            break
-
-
+    iter = iterate_each_value([1,2,3,4,5])
+    print(iter)
 
 main()
-
