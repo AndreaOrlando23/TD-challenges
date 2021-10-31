a9c29fb055625c01a9ad4f1fa094353d3ac16e89
AndreaOrlando23 <64850221+AndreaOrlando23@users.noreply.github.com>
2021-10-31 18:45:25 +0100

    fix: problem with importing modules. Not solved yet

--- python/caesar-cipher-challenge/crack-caesar-cipher-OOP/main.py
+++ python/caesar-cipher-challenge/crack-caesar-cipher-OOP/main.py
@@ -1,4 +1,5 @@
-from caesar_cipher_crack.crack import Crack
+# Entry point for Crack Caesar Chipher program
+from .caesar_cipher_crack.crack import Crack
 
 
 def main():
@@ -7,3 +8,10 @@ def main():
 
 if __name__ == '__main__':
     main()
+
+
+"""
+This script does not work yet.
+I have to solve problem with importing modules
+Test the program with main.py inside caesar_cipher_crack package
+"""
