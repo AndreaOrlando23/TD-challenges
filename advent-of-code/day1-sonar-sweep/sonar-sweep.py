cdc6deff253d05c056cfa262ebd9c2ab1b30b699
Andrea Orlando <andrea.orlando87@gmail.com>
2021-12-02 16:37:45 +0100

    feat: implement solution for day1/sonar-sweep AOC challenge - PART 2

--- advent-of-code/day1-sonar-sweep/sonar-sweep.py
+++ advent-of-code/day1-sonar-sweep/sonar-sweep.py
@@ -1,11 +1,22 @@
-with open('data.txt', 'r') as file:
-    lines = file.read().split()
-    print(lines)
+def num_increases(data):
     count = 0
-    
-    for i in range(1, len(lines)):
-        if lines[i] > lines[i-1]:
+    for i in range(1, len(data)):
+        if data[i] > data[i-1]:
             count += 1
-               
+    
+    return count
+
+
+with open('data.txt') as file:
+    values = [int(val) for val in file.read().split()]
+
         
-    print(f'Measurement larger then previous measurement: {count}')
+print(f'PART1 Sonar Sweep - Number of increases: {num_increases(values)}')
+
+
+three_measurement = []
+
+for i in range(2, len(values)):
+    three_measurement.append(values[i] + values[i-1] + values[i-2])
+
+print(f'PART2 Sonar Sweep - Number of increases: {num_increases(three_measurement)}')
