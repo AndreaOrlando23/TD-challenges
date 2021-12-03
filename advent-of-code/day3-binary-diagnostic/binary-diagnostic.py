169b96a1880a3b10d6bbb35b649c1f45d58f64bd
Andrea Orlando <andrea.orlando87@gmail.com>
2021-12-03 14:14:58 +0100

    feat: implement solution for PART2 AOC - binary diagnostic

--- advent-of-code/day3-binary-diagnostic/binary-diagnostic.py
+++ advent-of-code/day3-binary-diagnostic/binary-diagnostic.py
@@ -1,5 +1,3 @@
-# TODO
-
 def binary_diagnostic(binary_list):
     gamma = ''
     epsilon = ''
@@ -26,10 +24,60 @@ def binary_diagnostic(binary_list):
     return [g, e]
 
 
-
 with open('data.txt') as file:
-    binary_data = [val for val in file.read().split()]
+    data = [val for val in file.read().split()]
+    
+solution1 = binary_diagnostic(data)
+print(f'PART 1 Binary Diagnostic - Solution: {solution1[0]*solution1[1]}')  # 1458194
+
+data2 = data.copy()
+
+index = 0
+while len(data) > 1:
+    one = 0
+    zero = 0
+    ones = []
+    zeroes = []
+    for c in range(0, len(data)):
+        if data[c][index] == '0':
+            zero += 1
+            zeroes.append(data[c])
+        else:
+            one += 1
+            ones.append(data[c])
+    
+    if zero > one:
+        data = zeroes
+    else:
+        data = ones
+    index += 1
+    
+oxygen = int(data[0], 2)
 
 
-data = binary_diagnostic(binary_data)
-print(f'PART 1 Binary Diagnostic - Solution: {data[0]*data[1]}')
+data = data2
+index = 0
+while len(data) > 1:
+    one = 0
+    zero = 0
+    ones = []
+    zeroes = []
+    for c in range(0, len(data)):
+        if data[c][index] == '0':
+            zero += 1
+            zeroes.append(data[c])
+        else:
+            one += 1
+            ones.append(data[c])
+    
+    if one < zero:
+        data = ones
+    else:
+        data = zeroes
+    
+    index += 1
+    
+co2 = int(data[0], 2)
+
+solution2 = oxygen*co2
+print(f'PART 2 Binary Diagnostic - Solution: {solution2}')  # 2829354
